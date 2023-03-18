d672b62acd4c96ce5c3a7c0fedc3a27e50ff56f8
Muhammad Nabih <86538085+GOKERIAN@users.noreply.github.com>
2023-03-18 22:08:09 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+AOS.init();
+let loader = document.getElementById("preloader");
+
+setTimeout(() => {
+  loader.style.display = "none";
+}, 5000);
+
+setTimeout(() => {
+  let typingText = new Typed("#text", {
+    strings: [
+      "في مكتبتي الالكترونية ",
+      "أنا سعيد بحضورك هنا",
+      "يمكنك أخذ جولة رائعة هنا ",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+  });
+}, 6000);
